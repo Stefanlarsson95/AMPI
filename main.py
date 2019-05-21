219fc15410d68940572228eccda27502113ffe97
Stefan <stefanlarsson95@gmail.com>
2019-05-21 18:52:25 +0200

    README: added amp temp comands

--- main.py
+++ main.py
@@ -14,24 +14,31 @@
 
 from luma.core.interface.serial import spi
 from luma.oled.device import ssd1322
-from oled import animated_gif, clock, font_awesome, sprite_animation, perfloop, greyscale, scrolling_pixelart, welcome, albumart, carousel
-import time
-import subprocess
+from oled import scrolling_pixelart
+import CppHeaderParser
 
 
 # oled device config
 device = ssd1322(spi(device=0, port=0))
 
+# ADAU1701 configs
 
+
+def print_header():
+    header = CppHeaderParser.CppHeader('/home/pi/Documents/Python/AMPI/SigmaStudio/AMPI_1_IC_1_PARAM.h')
+    #for defs in header.defines:
+        #print " %s" % defs
+    room = header.defines
+    print room[5]
 def main():
-
-    while True:
+    print_header()
+    #while True:
         #scrolling_pixelart.device = device
         #scrolling_pixelart.main()
         #albumart.device = device
         #albumart.main()
-        carousel.device=device
-        carousel.main()
+        #carousel.device=device
+        #carousel.main()
 
 
 if __name__ == "__main__":
